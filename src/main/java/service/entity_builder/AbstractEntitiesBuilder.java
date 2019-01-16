c8eb2f658771dfac2327865df9bf3644883d2d25
DenisPan007 <denispan007@gmail.com>
2019-01-16 16:51:09 +0300

    add class "AbstractEntitiesBuilder"

---
+++ src/main/java/service/entity_builder/AbstractEntitiesBuilder.java
@@ -0,0 +1,18 @@
+package service.entity_builder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class AbstractEntitiesBuilder<T> {
+    protected Set<T> entities;
+    public AbstractEntitiesBuilder() {
+        entities = new HashSet<T>();
+    }
+    public AbstractEntitiesBuilder(Set<T> entities) {
+        this.entities = entities;
+    }
+    public Set<T> getEntities() {
+        return entities;
+    }
+    abstract public void buildSetEntities(String fileName);
+}
