84c7cb6f3bac5496d1738c483339e4719a7c13fb
Nikita <bugati23@yandex.ru>
2019-01-17 01:28:30 +0300

    add entity

---
+++ src/main/java/entity/Device.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Device {
+    private String ID;
+    private List<Component> components;
+
+    public Device() {
+        components = new ArrayList<>();
+    }
+
+    public List<Component> getComponents() {
+        return components;
+    }
+
+    public void setComponents(List<Component> components) {
+        this.components = components;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Device device = (Device) o;
+        return Objects.equals(ID, device.ID) &&
+                Objects.equals(components, device.components);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID, components);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Device{");
+        sb.append(ID).append(' ');
+        sb.append(", components=").append(components);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/entity/Port.java
@@ -0,0 +1,5 @@
+package entity;
+
+public enum Port{
+    COM,USB,LPT
+}
